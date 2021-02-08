be24fa69f98564d50871d3e8784e488b07fa2a60
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2021-02-08 15:38:01 +0900

    Add SiteUserLoginView

--- app/views.py
+++ app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.views import View
 # Create your views here.
+
+class SiteUserLoginView(View):
+    def get(self, request, *args, **kwargs):
+        
+        return render(request, 'app/siteUser/login.html')
+
+site_user_login = SiteUserLoginView.as_view()
